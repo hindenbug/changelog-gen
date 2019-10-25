3c312294ce9507b7d49a834f70fce1e1a9fedfe9
Duilio Ruggiero <duilio.ruggiero@gmail.com>
2019-10-25 16:02:23 +0200

    use git-jurnal

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use gitjournal::GitJournal;
+use failure::Error;
+
+fn run() -> Result<(), Error> {
+    let mut journal = GitJournal::new("/Users/duilioruggiero/Documents/github/test/").unwrap();
+    journal
+        .parse_log("v1..v2", "nc", &0, &true, &true, None)
+        .expect("Could not parse log.");
+    journal
+        .print_log(true, None, None)
+        .expect("Could not print short log.");
+    journal
+        .print_log(false, None, None  )
+        .expect("Could not print detailed log.");
+    Ok(())
+}
+
 fn main() {
-    println!("Hello, world!");
+    match run() {
+        Ok(()) => println!("End!"),
+        Err(e) => println!("error: {}", e),
+    }
 }
